acc6f6397290b28833b1722b65ad3fe23d352321
kamy-rivera <camila.rivera.b@gmail.com>
2025-02-02 16:57:56 -0300

    Create firebaseconfig.js

---
+++ firebaseconfig.js
@@ -0,0 +1,10 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "TU_API_KEY",
+  authDomain: "TU_AUTH_DOMAIN",
+  projectId: "TU_PROJECT_ID",
+  storageBucket: "TU_STORAGE_BUCKET",
+  messagingSenderId: "TU_MESSAGING_SENDER_ID",
+  appId: "TU_APP_ID"
+};
+export const app = initializeApp(firebaseConfig);
